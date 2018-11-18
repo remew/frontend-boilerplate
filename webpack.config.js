58592ce6b0e89d1374802ae8047c246d72f6d000
remew <foolishremew@gmail.com>
2018-11-18 21:39:21 +0900

    use [hash].js instead of bundle.js for cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: './src/index.ts',
   output: {
-    filename: 'bundle.js',
+    filename: '[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
